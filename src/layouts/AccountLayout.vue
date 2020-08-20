43d54cd4c37d7f5b63b6812bdd3a881000559f4b
Carlos Hernandez <CarlosHdz_97@outlook.com>
2020-08-20 12:40:33 -0500

    Creación de la vista de modulos

---
+++ src/layouts/AccountLayout.vue
@@ -0,0 +1,21 @@
+<template>
+    <q-layout view="hHh lpR fFf">
+        <q-drawer show-if-above v-model="isConfig" side="right" elevated>
+        </q-drawer>
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+    </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'AccountLayout',
+  data () {
+    return {
+      isConfig: false,
+    }
+  }
+}
+</script>
---
+++ src/pages/Account/Index.vue
@@ -0,0 +1,54 @@
+<template>
+    <q-page>
+        <div class="row items-center q-py-md q-px-md">
+            <div class="col-12 text-center">
+                <q-avatar class="shadow-4" size="80px">
+                    <img src="https://cdn.quasar.dev/img/avatar.png">
+                </q-avatar>
+                <div class="text-h6">Carlos Hernández</div>
+                <div class="text-caption">ADMINISTRADOR</div>
+                <q-select dense outlined color="grey" v-model="wp" :options="workpoints" option-value="id" option-label="name" class="q-px-xl"/>
+            </div>
+        </div>
+        <div class="row q-col-gutter-lg justify-center">
+            <div class="col-5" v-for="module in modules" :key="module.name">
+                <q-card>
+                    <q-card-section class="text-center">
+                        <q-avatar :color="module.color" :text-color="module.text" :icon="module.icon"/> <!-- insert_photo -->
+                        <div class="text-subtitle2">{{module.name}}</div>
+                    </q-card-section>
+                </q-card>
+            </div>
+        </div>
+    </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+      return {
+            modules: [
+                {'name': 'Usuarios', 'icon': 'people_alt', 'color': 'cyan-1', 'text': 'cyan-6', 'path': '/account'},
+                {'name': 'Soporte', 'icon': 'support', 'color': 'deep-purple-1', 'text': 'deep-purple-10', 'path': '/suport'},
+                {'name': 'Productos', 'icon': 'trip_origin', 'color': 'blue-grey-1', 'text': 'blue-grey-9', 'path': '/products'},
+                {'name': 'Inventarios', 'icon': 'view_module', 'color': 'pink-1', 'text': 'pink-9', 'path': '/inventory'},
+                {'name': 'Almacen', 'icon': 'bento', 'color': 'yellow-1', 'text': 'yellow-10', 'path': '/celler'},
+                {'name': 'Etiquetas', 'icon': 'local_offer', 'color': 'light-green-1', 'text': 'light-green-9', 'path': '/labels'},
+                {'name': 'Preventa', 'icon': 'shopping_bag', 'color': 'teal-1', 'text': 'teal-9', 'path': '/preSale'},
+                {'name': 'Punto de venta', 'icon': 'store', 'color': 'orange-1', 'text': 'orange-10', 'path': '/sale'},
+            ],
+            workpoints: [
+                {'id': 1, 'name': 'CEDIS SAN PABLO'},
+                {'id': 2, 'name': 'CEDIS PANTACO'},
+                {'id': 3, 'name': 'SAN PABLO 1'},
+                {'id': 4, 'name': 'SAN PABLO 2'},
+                {'id': 5, 'name': 'SAN PABLO 3'},
+                {'id': 6, 'name': 'CORREO 1'},
+                {'id': 7, 'name': 'CORREO 2'},
+            ],
+            wp : null,
+      }
+  }
+}
+</script>
