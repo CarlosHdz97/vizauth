3c45c79f65a2c3e700c08655df7ecd2c973f71fa
Carlos Hernandez <CarlosHdz_97@outlook.com>
2020-08-24 12:20:05 -0500

    Implementación del mensaje de error en login y mostrar contraseña

--- src/pages/Auth/Login.vue
+++ src/pages/Auth/Login.vue
@@ -1,11 +1,18 @@
 <template>
     <q-page class="row flex-center justify-center">
-        <q-card class="col-11 col-md-4" flat>
-            <div class="text-weight-bold text-h4 text-center">VIZAPP</div>
-            <div class="text-subtitle2 text-center">¡Bienvenido!</div>
-            <q-form class="q-pa-md text-right">
+        <q-card class="col-11 col-md-4 text-center" flat>
+            <div class="text-weight-bold text-h4">VIZAPP</div>
+            <div class="text-subtitle1">¡Bienvenido!</div>
+            <div v-if="msg_err">
+                <q-chip color="red-10" text-color="white" icon="warning" :label="msg_err" />
+            </div>
+            <q-form class="q-pa-sm text-right">
                 <q-input v-model="form.nick" label="Usuario" color="black" required />
-                <q-input v-model="form.password" label="Contraseña" color="black" type="password" required />
+                <q-input v-model="form.password" label="Contraseña" color="black" :type="showingPassword ? 'text' : 'password'" required>
+                    <template v-slot:append>
+                        <q-icon :name="showingPassword ? 'visibility' : 'visibility_off'" class="cursor-pointer" @click="showingPassword = !showingPassword"/>
+                    </template>
+                </q-input>
                 <div class="text-overline text-grey-7">Olvide mi contraseña</div>
                 <q-btn type="submit" rounded="" class="full-width q-mt-lg" color="dark" label="Iniciar sesión"/>
             </q-form>
@@ -21,7 +28,9 @@ export default {
           form: {
               nick: '',
               password: ''
-          }
+          },
+          showingPassword: false,
+          msg_err: ''
       }
   }
 }
